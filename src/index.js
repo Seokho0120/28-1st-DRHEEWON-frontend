e732f05d57100224ec2b4fcc561a32cff85e5d97
KimJeongHyun <ptr1129@hanmail.net>
2021-12-27 20:40:30 +0900

    [Modify] index.scss, import 경로 수정

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import Router from './Router';
 
 import './styles/common.scss';
 import './styles/reset.scss';
+import './index.scss';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
