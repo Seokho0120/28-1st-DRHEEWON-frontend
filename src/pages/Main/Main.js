6a2b774e624acfda2398dfb9c9f2930a332f7afd
KimJeongHyun <ptr1129@hanmail.net>
2021-12-27 21:30:04 +0900

    [Modify] Main, Nav 기초 작성

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,11 @@
+import Nav from '../../components/Nav/Nav';
+
+import './Main.scss';
+
+export default function Main() {
+  return (
+    <main className="mainContainer">
+      <Nav />
+    </main>
+  );
+}
