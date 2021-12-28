d05a3db4d694698c344b9215dab1510903228075
KimJeongHyun <ptr1129@hanmail.net>
2021-12-28 09:35:29 +0900

    [Modify] Navigation component 위치 변경  - Nav bar가 모든 page에 적용되는 바 router의 wrapper로 적용  - 관련 태그 및 스타일 Router.js 및 index.scss로 이관

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
+import Nav from './components/Nav/Nav';
 
 export default function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </BrowserRouter>
+    <main className="headContainer">
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
