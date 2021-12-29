773f0e94487f60f42913d169df949e0b7f1d3b4e
KangHeeWon <kkhw1202@gmail.com>
2021-12-29 18:40:50 +0900

    Modify: Main 수정 및 router 추가

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
+// import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
