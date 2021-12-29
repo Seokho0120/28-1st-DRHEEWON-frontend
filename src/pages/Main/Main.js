9a340bdd41dcb57c73cf72fac1c6beda3495ed8f
KimJeongHyun <ptr1129@hanmail.net>
2021-12-29 15:22:23 +0900

    [Fix] 오 커밋된 페이지 롤백 - Main 부분 파일 삭제

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,5 @@
-import MainSlider from './MainSlider';
-
 import './Main.scss';
-import MainBestIcon from './MainBestIcon';
 
 export default function Main() {
-  return (
-    <div className="mainWrapper">
-      <MainSlider />
-      <div className="sectionMargin" />
-      <MainBestIcon />
-    </div>
-  );
+  return <>Main!</>;
 }
