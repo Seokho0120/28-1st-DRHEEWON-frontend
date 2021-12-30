8bf119abfb16f15e196149be979a40276b5fc190
seankim1111 <yongsun199@gmail.com>
2021-12-30 09:04:50 +0900

    [Add] List page 전체 레이아웃 및 컴포넌트 레이아웃잡기 및 스타일 적용

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Main from './pages/Main/Main';
+import ProductList from './pages/ProductList/ProductList.js';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/ProductList" element={<ProductList />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/ProductList/ProductList.js
@@ -0,0 +1,125 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faList,
+  faChevronDown,
+  faHeart,
+} from '@fortawesome/free-solid-svg-icons';
+
+import dummyImg from '../../styles/dummyImg.JPG';
+import './ProductList.scss';
+
+export default function ProductList() {
+  return (
+    <body>
+      <div className="productList">
+        {/* <header /> */}
+        <header>
+          <div>뒤로가기키</div>
+          <div className="subHeadBottom">
+            <h1>전체 보기</h1>
+            <p>100개의 남성 전체 보기 상품이 있습니다.</p>
+          </div>
+        </header>
+        <main>
+          {/* <headContainer /> */}
+          <section className="headContainer">
+            <article className="layoutWrapper">
+              {/* 변수명 수정 예정 */}
+              <div className="container">
+                <button id="firstCategory">슈즈</button>
+                <button id="secondCategory">부츠</button>
+                <button id="thirdCategory">전체 보기</button>
+              </div>
+              <div className="containerDetail">
+                남성 부츠 컬렉션입니다. 남성 부츠는 수십년 동안 자기 자신의 표현
+                수단으로서 자리잡았습니다. 클래식한 첼시 부츠부터 독보적인
+                오리지널 부츠, 얼터너티브한 디자인까지 다양한 스타일을
+                만나보세요.
+              </div>
+              <div className="filterButtonWrapper">
+                <button className="filterButton">
+                  <span className="filterButtonList">
+                    <FontAwesomeIcon icon={faList} />
+                  </span>
+                  <span className="filterButtonTxt">필터</span>
+                  <span className="filterButtonArrow">
+                    <FontAwesomeIcon icon={faChevronDown} />
+                  </span>
+                </button>
+              </div>
+            </article>
+            {/* 필터창 구현예정
+                click event 발생시 opacity 변동으로 인한 필터창 출현 */}
+            <article className="filterContainer">
+              <div className="filterLayoutWrapper">
+                <form action="#!" className="filterListContainer_form">
+                  <div>정렬</div>
+                  <div>사이즈</div>
+                  <div>가격</div>
+                  <div>색상</div>
+                </form>
+                <div className="filterListContainer_body">
+                  <button>초기화</button>
+                  <button>적용</button>
+                </div>
+              </div>
+            </article>
+          </section>
+          {/* <mainContainer /> */}
+          <section className="mainContainer">
+            <article className="mainContainerWrapper">
+              <ul className="productList">
+                <li className="item">
+                  <div className="itemImage">
+                    <img src={dummyImg.JPG} alt="더미이미지" />
+                  </div>
+                  <div className="itemRow">
+                    <div className="itemColumnLeft">
+                      <h2 className="title">상품 이름</h2>
+                      <div className="additionalInfo">
+                        <p>
+                          <span>아이템 설명</span>
+                        </p>
+                      </div>
+                      <button className="heartButton">
+                        <FontAwesomeIcon icon={faHeart} />
+                        4444
+                      </button>
+                    </div>
+                    <div className="itemColumnRight">
+                      <div className="priceWrapper">3000000원</div>
+                      <button className="cartButton">
+                        <span className="cartButtonText">장바구니담기</span>
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+              <div />
+            </article>
+          </section>
+        </main>
+        {/* <popularKeyword /> */}
+        <footer>
+          <section className="popularKeyword">
+            <article className="keywordHead">
+              <h2>인기 키워드</h2>
+              <p>당신에게 맞는 키워드를 클릭해 보세요.</p>
+            </article>
+            <ul className="keywordList">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+              <li>6</li>
+              <li>7</li>
+            </ul>
+          </section>
+        </footer>
+      </div>
+    </body>
+  );
+}
