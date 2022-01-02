891b68e928fb7e47d4c0936a8de00aaad3e58c59
KimJeongHyun <ptr1129@hanmail.net>
2022-01-02 19:53:12 +0900

    [Add] Mockdata DB 대비 json으로 분기

--- src/pages/Main/MainFlexSliderFetch.js
+++ src/pages/Main/MainFlexSliderFetch.js
@@ -1,16 +1,26 @@
 import { useState, useEffect } from 'react';
 
 import { FlexibleSlider } from '../../components/FlexibleSlider/FlexibleSlider';
-import { bestIconContentsData, mdRecommendContentsData } from './MainMockData';
+// import { bestIconContentsData, mdRecommendContentsData } from './MainMockData';
 
 export default function MainFlexSliderFetch({ sectionArea, selectedIcon }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    sectionArea === 'bestIcon'
-      ? setProducts(bestIconContentsData)
-      : setProducts(mdRecommendContentsData);
-  }, [sectionArea]);
+    switch (sectionArea) {
+      case 'bestIcon':
+        fetch(`http://localhost:3000/data/bestIcon${selectedIcon}.json`)
+          .then(res => res.json())
+          .then(data => setProducts(data));
+        break;
+      case 'mdRecommend':
+        fetch(`http://localhost:3000/data/mdRecommend${selectedIcon}.json`)
+          .then(res => res.json())
+          .then(data => setProducts(data));
+        break;
+      default:
+    }
+  }, [sectionArea, selectedIcon]);
 
   return <FlexibleSlider sectionArea={sectionArea} products={products} />;
 }
