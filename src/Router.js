29273c14c8b422d98e4367b8eddc0a93d1ae6602
KangHeeWon <kkhw1202@gmail.com>
2022-01-04 00:14:14 +0900

    Add: 초기 셋팅(first commit)

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Nav from './components/Nav/Nav';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import Cart from './pages/Cart/Cart';
 
 export default function Router() {
   const [windowY, setWindowY] = useState();
@@ -30,6 +31,7 @@ export default function Router() {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+import ContentHeader from '../../components/RegisterLogin/ContentHeader';
+
+import './Cart.scss';
+import './../../components/RegisterLogin/RegisterLogin.scss';
+
+const Cart = () => {
+  return (
+    <div className="cartContainer">
+      <ContentHeader pageInfo="장바구니" />
+      <main>
+        <section className="choiceHandler">choice section</section>
+        <section className="cartListWrap">
+          <ul>
+            <li>product</li>
+          </ul>
+        </section>
+        <section className="orderPrice">order price</section>
+      </main>
+    </div>
+  );
+};
+
+export default Cart;
