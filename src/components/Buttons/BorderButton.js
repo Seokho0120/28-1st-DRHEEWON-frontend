8c6e23d4261935ef9bc1b7565b5fc6b4c32a559f
KimJeongHyun <ptr1129@hanmail.net>
2022-01-03 20:33:40 +0900

    [Modify] merge 후처리 (common.scss 수정, 미사용 패키지 처리 등) - common.scss smooth behavior 위치 이동 - 전역 color:$black에 대응하게끔 기존 Main, MainMixin.scss 공사 진행 - throttle 패키지 삭제, custom throttle 함수 적용 - 필요없는 BorderButton style 삭제

--- src/components/Buttons/BorderButton.js
+++ src/components/Buttons/BorderButton.js
@@ -4,56 +4,48 @@ const borderBtnList = {
   goMoreWhite: {
     fontSize: '24px',
     border: '0.5px solid white',
-    color: '#fff',
     text: '자세히 보기 >',
     padding: '15px 30px',
   },
   goMoreBlack: {
     fontSize: '24px',
     border: '0.5px solid black',
-    color: 'black',
     text: '자세히 보기 >',
     padding: '15px 30px',
   },
   putCart: {
     fontSize: '18px',
     border: '1px solid black',
-    color: 'black',
     text: '장바구니 담기',
     padding: '10px 17px',
   },
   longBoots: {
     fontSize: '18px',
     border: '1px solid black',
-    color: 'black',
     text: '롱 부츠',
     padding: '10px 25px',
   },
   collabo: {
     fontSize: '18px',
     border: '1px solid black',
-    color: 'black',
     text: '콜라보레이션',
     padding: '10px 25px',
   },
   furBoots: {
     fontSize: '18px',
     border: '1px solid black',
-    color: 'black',
     text: 'FUR부츠',
     padding: '10px 25px',
   },
   accGoods: {
     fontSize: '18px',
     border: '1px solid black',
-    color: 'black',
     text: 'ACC용품',
     padding: '10px 25px',
   },
   join: {
     fontSize: '18px',
     border: 'none',
-    color: '#191919',
     text: '회원가입',
     padding: '20px 60px',
     background: 'yellow',
@@ -79,7 +71,6 @@ export function BorderButton({ type, btnName, selectedIcon, onClickMethod }) {
         className={type.includes('goMoreBlack') ? 'black' : ''}
         style={{
           fontSize: borderBtnList[type].fontSize,
-          color: borderBtnList[type].color,
           padding: borderBtnList[type].padding,
           pointerEvents: !type.includes('goMore') ? 'none' : '',
         }}
