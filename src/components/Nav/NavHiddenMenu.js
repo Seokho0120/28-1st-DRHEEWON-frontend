8cbc0f9f8a5f14611d09b9402fe098a0714f4d1d
KimJeongHyun <ptr1129@hanmail.net>
2022-01-04 20:16:11 +0900

    [Fix] sider issue 해결

--- src/components/Nav/NavHiddenMenu.js
+++ src/components/Nav/NavHiddenMenu.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import useScrollHidden from '../../hooks/useScrollHidden';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
